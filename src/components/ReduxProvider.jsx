972b8b2aba9080db66e08ead3b9f4df1fb19be71
scffs <khidey@inbox.ru>
2023-05-20 00:56:18 +0700

    using Loading component

--- src/components/ReduxProvider.jsx
+++ src/components/ReduxProvider.jsx
@@ -1,13 +1,15 @@
 import React, { Suspense } from 'react';
 import { Provider } from 'react-redux';
 
+import Loading from './Loading.jsx';
+
 import App from '../App.jsx';
 
 import store from '../store/store.js';
 
 const ReduxProvider = () => {
   return (
-    <Suspense fallback={<div>Loading</div>}>
+    <Suspense fallback={<Loading />}>
       <Provider store={store}>
         <App />
       </Provider>
